fd912f167a686159a5d69965a4f58f11b2c6d6d9
RebecaMFep <martinezfeliperebeca@gmail.com>
2023-12-20 22:32:50 +0100

    added Movie Repository

--- src/main/java/dev/rebecamf/apimovies/repositories/MovieRepository.java
+++ src/main/java/dev/rebecamf/apimovies/repositories/MovieRepository.java
@@ -1,6 +1,7 @@
 package dev.rebecamf.apimovies.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import dev.rebecamf.apimovies.models.Movie;
 
 public interface MovieRepository extends JpaRepository<Movie,Long> {
     
