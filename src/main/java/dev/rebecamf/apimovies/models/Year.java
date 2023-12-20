1a3292dcb3d1ecdf44c67dcd4aad0b7c5ca2d556
RebecaMFep <martinezfeliperebeca@gmail.com>
2023-12-20 22:30:09 +0100

    added Year Model

--- src/main/java/dev/rebecamf/apimovies/models/Year.java
+++ src/main/java/dev/rebecamf/apimovies/models/Year.java
@@ -1,27 +1,5 @@
 package dev.rebecamf.apimovies.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GenerationValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-import jakarta.persistence.GenerationType;
-
-@Entity
-@Table (name = "years")
-
-public class Year {
-@Id
-@GeneratedValue (strategy = GenerationType.IDENTITY) @Colum (name = "id_year") 
-
-private Long id;
-
-}
-
-//luis
-
-y ahora YEAR
 import java.util.Set;
 
 import jakarta.persistence.Column;
@@ -34,38 +12,37 @@ import jakarta.persistence.GenerationType;
 
 @Entity
 @Table (name = "years")
-
 public class Year {
-@Id
-@GeneratedValue (strategy = GenerationType.IDENTITY) @Column (name = "id_year")
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY) @Colum (name = "id_year") 
+    private Long id;
 
-private Long id;
+    private Long creation_year;
 
-private Long creation_year;
+    @OneToMany(mappedBy = "creation_year")
+    private Set <Movie> movies;
 
-@OneToMany(mappedBy = "creation_year")
-private Set <Movie> movies;
+    public Year() {
+    }
 
-public Year() {
-}
+    public Year(Long creation_year) {
+      this.creation_year = creation_year;
+    }
 
-public Year(Long creation_year) {
-  this.creation_year = creation_year;
-}
-public Long getId() {
-  return id;
-}
+    public Long getId() {
+      return id;
+    }
 
-public void setId(Long id) {
-  this.id = id;
-}
+    public void setId(Long id) {
+      this.id = id;
+    }
 
-public Long getCreation_year() {
-  return creation_year;
-}
+    public Long getCreation_year() {
+      return creation_year;
+    }
 
-public void setCreation_year(Long creation_year) {
-  this.creation_year = creation_year;
-}
+    public void setCreation_year(Long creation_year) {
+      this.creation_year = creation_year;
+    }
 
 }
