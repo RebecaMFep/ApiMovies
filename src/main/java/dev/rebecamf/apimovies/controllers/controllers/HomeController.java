d0294357cde49204ba2db088ffbaf7c016b7b753
RebecaMFep <martinezfeliperebeca@gmail.com>
2023-12-20 21:59:24 +0100

    added HomeController

--- src/main/java/dev/rebecamf/apimovies/controllers/controllers/HomeController.java
+++ src/main/java/dev/rebecamf/apimovies/controllers/controllers/HomeController.java
@@ -4,10 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 public class HomeController {
-    @GetMapping(path = "/")
 
+    @GetMapping(path = "/")
     public String index() {
         
         return "Hello Springboot";
